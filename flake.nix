e8b820f41a8dd5810ba9a67010f20fedc2889fe6
Dakota <dakotak@outlook.com>
2024-06-19 09:04:50 -0700

    initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+    description = "Dakotak's Nix Configuration";
+
+    inputs = {
+        nixpkgs.url = "nixpkgs/nixos-23.11";
+
+        home-manager = {
+            url = "github:nix-community/home-manager/release-23.11";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = { self, nixpkgs, home-manager, ...} @ inputs:
+    let
+        inherit (self) outputs;
+        inherit (nixpkgs) lib;
+        specialArgs = { inherit inputs outputs nixpkgs; };
+    in
+    {
+        nixosConfigurations = {
+            xone = lib.nixosSystem {
+                inherit specialArgs;
+                system = "x86_64-linux";
+                modules = [
+                    # ./nix/hosts/xone
+                    ./configuration.nix
+                ];
+            };
+        };
+    };
+}
