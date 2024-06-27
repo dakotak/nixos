9677131446abf4ebd155b40e72564dfec159459c
Dakota <dakotak@outlook.com>
2024-06-27 12:41:16 -0700

    More hyprland

--- hosts/xone/default.nix
+++ hosts/xone/default.nix
@@ -121,7 +121,10 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    (waybar.overrideAttrs (oldAttrs: {
+      mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+    }))
+
     bat
     curl
     fzf
